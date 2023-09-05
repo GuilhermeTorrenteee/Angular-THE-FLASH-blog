65f921b5f230eecb4de3d6e5eea64e15e990d002
Guilherme Torrente <guilhermetorrente@outlook.com>
2023-09-05 05:53:05 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [{
+ "id": "1",
+  "title": "Como termina The Flash?",
+  "description": "De maneira sucinta, o final de The Flash traz um grande confronto entre Barry Allen e Eddie Thawne, criador da Força de Aceleração Negativa (que se opõe à Força de Aceleração de Allen) que, posteriormente, se tonou o vilão Azul-Cobalto.",
+  "photoCover":"https://img.ibxk.com.br/2023/05/25/25111203691074.jpg?ims=704x"
+},
+{
+  "id": "2",
+   "title": "The Flash: os 10 melhores episódios da série!",
+   "description": "Em 24 de maio deste ano, a série The Flash vai exibir seu episódio final. Depois de nove temporadas, a produção inserida no Arrowverse da DC e protagonizada por Grant Gustin no papel do veloz Barry Allen promete um desfecho emocionante, contando com a participação de figuras bastante celebradas pelos fãs ao longo dos anos.Porém, antes mesmo que esse fim seja transmitido, que tal relembrar alguns dos momentos mais icônicos da série? Para isso, recorremos ao Internet Movie Database (IMDb), que nos revelou quais são os melhores episódios de The Flash, de acordo com as avaliações feitas pelos usuários.",
+   "photoCover":"https://img.ibxk.com.br/2023/05/19/19173525120320.jpg?ims=704x"
+ },
+ {
+  "id": "3",
+   "title": "Com o fim da série The Flash, o Arrowverse acabou?",
+   "description": "De maneira sucinta, o final de The Flash traz um grande confronto entre Barry Allen e Eddie Thawne, criador da Força de Aceleração Negativa (que se opõe à Força de Aceleração de Allen) que, posteriormente, se tonou o vilão Azul-Cobalto.Como você deve se lembrar, durante a primeira temporada da série, Eddie atirou em seu próprio coração para evitar que seu descendente, o terrível Eobard Thawne, nascesse. Contudo, Eobard conseguiu ganhar vida através da Força de Aceleração Negativa, trazendo ao mundo um Eddie vingativo e cruel (incorporado por Azul-Cobalto), cujo objetivo era destruir Barry.",
+   "photoCover":"https://tm.ibxk.com.br/2023/05/25/25104106378056.jpg?ims=704x264"
+ },
+ {
+  "id": "4",
+   "title": "The Flash: Grant Gustin diz que aceitaria interpretar outro herói",
+   "description": "Em entrevista recente ao site Entertainment Weekly, Gustin revelou que não só voltaria a interpretar o Flash, caso tenha a oportunidade, como também não negaria papéis de outros super-heróis na televisão e no cinema. Não direi que estou me aposentando para sempre. Não sei o que o futuro reserva. [...], disse o ator. Muitas pessoas me perguntavam o que eu ia querer fazer após o fim desse capítulo, achando que eu não ia querer fazer coisas de heróis novamente, mas, não, eu virei fã desse gênero. Eu amo esse gênero. [...] Então, eu estaria aberto para fazer coisas de heróis de novo, Flash ou outra coisa, contanto que fosse um personagem empolgante. Adoraria ser chamado para algo relacionado ao Flash no futuro, completou o intérprete do Flash.",
+   "photoCover":"https://tm.ibxk.com.br/2023/05/23/23121710756107.jpg?ims=704x264"
+ }
+]
+
